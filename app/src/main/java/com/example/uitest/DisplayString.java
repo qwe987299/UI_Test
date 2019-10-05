325bf05be31693823256e799b8db4729ea84f18b
Feng, Cheng-Chi <qwe987299@gmail.com>
2019-10-05 16:06:54 +0800

    第二版

--- app/src/main/java/com/example/uitest/DisplayString.java
+++ app/src/main/java/com/example/uitest/DisplayString.java
@@ -2,6 +2,7 @@ package com.example.uitest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
 
@@ -10,8 +11,10 @@ public class DisplayString extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Intent intent = getIntent();
+        String outputString = intent.getStringExtra("userInputString");
         setContentView(R.layout.layout_display_string);
         TextView showStringTV = (TextView) findViewById(R.id.showString);
-        showStringTV.setText("Test");
+        showStringTV.setText(outputString);
     }
 }
--- app/src/main/java/com/example/uitest/MainActivity.java
+++ app/src/main/java/com/example/uitest/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +20,12 @@ public class MainActivity extends AppCompatActivity {
         //((Button) view).setText("OK");
 
         Intent intent = new Intent(this, DisplayString.class);
+        EditText editText = (EditText) findViewById(R.id.inputString);
+        EditText editText2 = (EditText) findViewById(R.id.inputString2);
+        String string2send = editText.getText().toString();
+        String string2send2 = editText2.getText().toString();
+        String result = string2send + string2send2;
+        intent.putExtra("userInputString", result);
         startActivity(intent);
 
     }
